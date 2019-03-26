76addbad571bb750faec04ecfefa5ef40bfc4d17
Erik Englund <englundGiant@gmail.com>
2019-03-25 23:10:29 -0500

    Initial commit.

---
+++ japicc-annotations/src/main/java/com/efenglu/japicc/annotations/SkipComplianceCheck.java
@@ -0,0 +1,12 @@
+package com.efenglu.japicc.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(value = {ElementType.TYPE})
+public @interface SkipComplianceCheck {
+
+}
